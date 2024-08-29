b6a01c6f79f2469c8df26c9465a827e993f9c3a5
Praveen De Silva <desilvabkp.23@uom.lk>
2024-08-29 06:43:52 +0530

    if

--- Set .intersection().py
+++ Set .intersection().py
@@ -1,13 +1,14 @@
-# Input :
-n = int(input())
-studentsEng = set(input().strip().split())
+if __name__ == '__main__':
+    # Input :
+    n = int(input())
+    studentsEng = set(input().strip().split())
 
-b = int(input())
-studentsFrn = set(input().strip().split())
+    b = int(input())
+    studentsFrn = set(input().strip().split())
 
 
-studentsBoth = studentsEng & studentsFrn
-noStudentsBoth = len(studentsBoth)
+    studentsBoth = studentsEng & studentsFrn
+    noStudentsBoth = len(studentsBoth)
 
-# Output :
-print(noStudentsBoth)
+    # Output :
+    print(noStudentsBoth)
