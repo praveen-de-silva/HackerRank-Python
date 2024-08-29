46cf9ac8f6b964273801b2ab4e345158f3463f33
Praveen De Silva <desilvabkp.23@uom.lk>
2024-08-29 06:43:12 +0530

    set .intersection()

---
+++ Set .intersection().py
@@ -0,0 +1,13 @@
+# Input :
+n = int(input())
+studentsEng = set(input().strip().split())
+
+b = int(input())
+studentsFrn = set(input().strip().split())
+
+
+studentsBoth = studentsEng & studentsFrn
+noStudentsBoth = len(studentsBoth)
+
+# Output :
+print(noStudentsBoth)
